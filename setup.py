6286c107a8990bc46c7708525484d4f17ad5e27e
kma003 <kmanderson003@gmail.com>
2024-08-19 13:47:21 -0700

    Updated gitignore and setup, added unet2d model used in base training script

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
+import os
 from setuptools import setup, find_packages
+
+# Set up data directories
+data_dirs = [
+    "data/"
+    "data/crests"
+]
+
+for d in data_dirs:
+    os.makedirs(d, exist_ok=True)
+
+requirements = [
+    "pandas",
+    "torch",
+    "torchvision",
+    "tqdm",
+    "wandb",
+]
 
 setup(
     name='soccer_crests_diffusion',
